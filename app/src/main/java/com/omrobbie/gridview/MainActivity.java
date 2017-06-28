1b84d2e97357d1b98949a5eb13c0de332f86e1e2
omrobbie <putu.roby@yahoo.com>
2017-06-28 14:14:04 +0800

    contoh penggunaan gridview

---
+++ app/src/main/java/com/omrobbie/gridview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.omrobbie.gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+    }
+}
